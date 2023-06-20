57d500e11b3d40c286d6551f9f007f5d7014c879
ton1czech <tonyasek007@gmail.com>
2023-06-20 08:09:14 +0200

    🎁NEW: container component for limited width

---
+++ components/Container.tsx
@@ -0,0 +1,16 @@
+interface ContainerProps {
+  children: React.ReactNode
+  styles?: string
+}
+
+const Container: React.FC<ContainerProps> = ({ children, styles }) => {
+  return (
+    <div
+      className={`2xl:max-w-screen-2xl xl:max-w-screen-xl md:mx-auto p-5 md:p-0 ${styles}`}
+    >
+      {children}
+    </div>
+  )
+}
+
+export default Container
