97b2a3837fc3b1a6af2d99d971c287228537114f
ton1czech <tonyasek007@gmail.com>
2023-06-20 08:53:08 +0200

    🎁NEW: test /eshop

---
+++ app/eshop/page.tsx
@@ -0,0 +1,18 @@
+import Container from '@/components/Container'
+
+export const metadata = {
+  title: '🐔 Farma Hájek - Eshop',
+  description: '',
+}
+
+const Eshop = () => {
+  return (
+    <div className='min-h-[200vh] bg-green-900 pt-20'>
+      <Container>
+        <h1 className='text-5xl'>ESHOOOP</h1>
+      </Container>
+    </div>
+  )
+}
+
+export default Eshop
