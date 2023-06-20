277f68c7a312562beff8e645d3ca26586ced9236
ton1czech <tonyasek007@gmail.com>
2023-06-20 08:52:19 +0200

    🎁NEW: hero

---
+++ components/Hero.tsx
@@ -0,0 +1,27 @@
+import Container from './Container'
+
+const Hero = () => {
+  return (
+    <header>
+      <div className='sticky top-0 -z-10 flex min-h-screen'>
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline
+          className='absolute h-full w-full object-cover'
+        >
+          <source src='/hero.webm' type='video/webm' />
+        </video>
+        <div className='absolute bg-black/75 top-0 left-0 w-full h-full'></div>
+        <Container styles='z-10 h-full w-full'>
+          <div className='pt-20'>
+            <h1 className='bg-white text-4xl font-black'>ahhoooooj</h1>
+          </div>
+        </Container>
+      </div>
+    </header>
+  )
+}
+
+export default Hero
