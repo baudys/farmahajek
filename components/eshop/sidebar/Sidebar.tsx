6fc242d85fea0ce1b0ccd7be80136705f2602469
ton1czech <tonyasek007@gmail.com>
2023-06-21 17:06:15 +0200

    🎁NEW: eshop sidebar

---
+++ components/eshop/sidebar/Sidebar.tsx
@@ -0,0 +1,80 @@
+'use client'
+
+import useSelectCategory from '@/hooks/useSelectCategory'
+import Link from 'next/link'
+import { useEffect } from 'react'
+
+const Sidebar: React.FC = () => {
+  const selectCategory = useSelectCategory(state => state.set)
+  const selectedCategory = useSelectCategory(state => state.selected)
+
+  const pathname = window.location.pathname
+
+  useEffect(() => {
+    if (pathname === '/eshop') selectCategory('vse')
+    if (pathname === '/eshop/slepicky') selectCategory('slepicky')
+    if (pathname === '/eshop/kurniky') selectCategory('kurniky')
+    if (pathname === '/eshop/smesi') selectCategory('smesi')
+    if (pathname === '/eshop/doplnky') selectCategory('doplnky')
+  }, [pathname, selectCategory])
+
+  return (
+    <div className='flex flex-col gap-20 items-center mb-16'>
+      <input
+        type='text'
+        placeholder='hledej'
+        className='bg-zinc-200 p-1 border-2 border-zinc-400 w-full rounded-lg'
+      />
+
+      <ul className='flex flex-col self-start gap-2 w-full text-center'>
+        <Link
+          href='/eshop'
+          onClick={() => selectCategory('vse')}
+          className={`rounded-lg border-2 border-black py-1 px-4 w-full ${
+            selectedCategory === 'vse' && 'bg-black text-white'
+          }`}
+        >
+          Vše
+        </Link>
+        <Link
+          href='/eshop/slepicky'
+          onClick={() => selectCategory('slepicky')}
+          className={`rounded-lg border-2 border-black py-1 px-4 w-full ${
+            selectedCategory === 'slepicky' && 'bg-black text-white'
+          }`}
+        >
+          Slepičky
+        </Link>
+        <Link
+          href='/eshop/kurniky'
+          onClick={() => selectCategory('kurniky')}
+          className={`rounded-lg border-2 border-black py-1 px-4 w-full ${
+            selectedCategory === 'kurniky' && 'bg-black text-white'
+          }`}
+        >
+          Kurníky
+        </Link>
+        <Link
+          href='/eshop/smesi'
+          onClick={() => selectCategory('smesi')}
+          className={`rounded-lg border-2 border-black py-1 px-4 w-full ${
+            selectedCategory === 'smesi' && 'bg-black text-white'
+          }`}
+        >
+          Směsi
+        </Link>
+        <Link
+          href='/eshop/doplnky'
+          onClick={() => selectCategory('doplnky')}
+          className={`rounded-lg border-2 border-black py-1 px-4 w-full ${
+            selectedCategory === 'doplnky' && 'bg-black text-white'
+          }`}
+        >
+          Doplňky
+        </Link>
+      </ul>
+    </div>
+  )
+}
+
+export default Sidebar
