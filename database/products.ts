27b146bf750f1a33551ed2a7d1f1c727b8f7495b
ton1czech <tonyasek007@gmail.com>
2023-06-23 16:59:01 +0200

    🎁NEW: kurniky database

---
+++ database/products.ts
@@ -0,0 +1,74 @@
+export const kurniky = [
+  {
+    href: '/eshop/kurniky/kurnik-6-netepleny/',
+    label: 'Kurník na 6 slepic neteplený',
+    src: '/kurniky/kurnik-6-netepleny/1.jpg',
+    price: 9500,
+  },
+  {
+    href: '/eshop/kurniky/kurnik-20-tepleny/',
+    label: 'Kurník na 20 slepic teplený',
+    src: '/kurniky/kurnik-20-tepleny/1.jpg',
+    price: 45980,
+  },
+  {
+    href: '/eshop/kurniky/kurnik-10-tepleny',
+    label: 'Kurník na 10 slepic teplený',
+    src: '/kurniky/kurnik-10-tepleny/1.jpg',
+    price: 39930,
+  },
+  {
+    href: '/eshop/kurniky/kurnik-20-netepleny',
+    label: 'Kurník na 20 slepic neteplený',
+    src: '/kurniky/kurnik-20-netepleny/1.jpg',
+    price: 27830,
+  },
+  {
+    href: '/eshop/kurniky/kurnik-10-netepleny',
+    label: 'Kurník na 10 slepic neteplený',
+    src: '/kurniky/kurnik-10-netepleny/1.jpg',
+    price: 24200,
+  },
+  {
+    href: '/eshop/kurniky/kurnik-terasovy',
+    label: 'Kurník terasový / balkonový s trusníkem',
+    src: '/kurniky/kurnik-terasovy/1.jpg',
+    price: 15125,
+  },
+  {
+    href: '/eshop/kurniky/vybeh-bez-vstupu',
+    label: 'Uzavřený výběh bez vstupu 1/2 krytý',
+    src: '/kurniky/vybeh-bez-vstupu/1.jpg',
+    price: 7865,
+  },
+  {
+    href: '/eshop/kurniky/vybeh-se-vstupem',
+    label: 'Uzavřený výběh se vstupem',
+    src: '/kurniky/vybeh-se-vstupem/1.jpg',
+    price: 15125,
+  },
+  {
+    href: '/eshop/kurniky/kralikarna-6',
+    label: 'Králíkárna 6 kotcová',
+    src: '/kurniky/kralikarna-6/1.jpg',
+    price: 30250,
+  },
+  {
+    href: '/eshop/kurniky/kralikarna-4',
+    label: 'Králíkárna 4 kotcová',
+    src: '/kurniky/kralikarna-4/1.jpg',
+    price: 22990,
+  },
+  {
+    href: '/eshop/kurniky/bedna-preprava',
+    label: 'Bedna na přepravu slepic',
+    src: '/kurniky/bedna-preprava/1.jpg',
+    price: 800,
+  },
+  {
+    href: '/eshop/kurniky/vybeh',
+    label: 'Výběh',
+    src: '/kurniky/vybeh/1.jpg',
+    price: 10890,
+  },
+]
