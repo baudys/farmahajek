c708b6b2fc6a64e0460eec6802de8fb185cdd012
ton1czech <tonyasek007@gmail.com>
2023-06-23 17:27:56 +0200

    🎁NEW: subheadings hidden when no match

--- app/eshop/page.tsx
+++ app/eshop/page.tsx
@@ -1,9 +1,18 @@
+'use client'
+
 import Container from '@/components/Container'
 import Sidebar from '@/components/eshop/sidebar/Sidebar'
 import Gallery from '@/components/eshop/Gallery'
 import { kurniky, slepicky } from '@/database/products'
+import useSearchBar from '@/hooks/useSearchBar'
+import { getFilteredProducts } from '@/lib/getFilteredProducts'
 
 const Eshop = () => {
+  const query = useSearchBar(state => state.query)
+
+  const filteredSlepicky = getFilteredProducts(query, slepicky)
+  const filteredKurniky = getFilteredProducts(query, kurniky)
+
   return (
     <div className='bg-white py-20'>
       <h2 className='uppercase text-white text-5xl text-center bg-brown font-bold py-20 mb-8'>
@@ -14,25 +23,29 @@ const Eshop = () => {
           <Sidebar />
 
           <div>
-            <div>
-              <div className='flex items-center gap-4 mb-2'>
-                <h4 className='relative uppercase font-semibold text-2xl text-brown'>
-                  Slepičky
-                </h4>
-                <div className='bg-brown w-full h-[2px]' />
+            {filteredSlepicky.length !== 0 && (
+              <div className='mb-48'>
+                <div className='flex items-center gap-4 pb-2'>
+                  <h4 className='relative uppercase font-semibold text-2xl text-brown'>
+                    Slepičky
+                  </h4>
+                  <div className='bg-brown w-full h-[2px]' />
+                </div>
+                <Gallery data={slepicky} isSlepice />
               </div>
-              <Gallery data={slepicky} isSlepice />
-            </div>
+            )}
 
-            <div>
-              <div className='flex items-center gap-4 mt-48 mb-2'>
-                <h4 className='relative uppercase font-semibold text-2xl text-brown'>
-                  Kurníky
-                </h4>
-                <div className='bg-brown w-full h-[2px]' />
+            {filteredKurniky.length !== 0 && (
+              <div className='mb-48'>
+                <div className='flex items-center gap-4 pb-2'>
+                  <h4 className='relative uppercase font-semibold text-2xl text-brown'>
+                    Kurníky
+                  </h4>
+                  <div className='bg-brown w-full h-[2px]' />
+                </div>
+                <Gallery data={kurniky} />
               </div>
-              <Gallery data={kurniky} />
-            </div>
+            )}
           </div>
         </div>
       </Container>
