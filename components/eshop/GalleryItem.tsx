d212f79eb12e4c76b0288197ca4ed97249bfefbf
ton1czech <tonyasek007@gmail.com>
2023-06-25 00:08:02 +0200

    👌IMPROVE: gallery item ui

--- components/eshop/GalleryItem.tsx
+++ components/eshop/GalleryItem.tsx
@@ -18,31 +18,32 @@ const GalleryItem: React.FC<GalleryItemProps> = ({
   transparentBg,
 }) => {
   return (
-    <Link
-      href={href}
-      className='group rounded-md hover:ring-2 hover:ring-zinc-200 trasnition'
-    >
-      <div
-        className={`bg-zinc-200 group-hover:bg-zinc-300 transition rounded-t-md h-56 overflow-hidden ${
-          transparentBg && 'grid place-content-center'
-        }`}
-      >
-        <Image
-          src={src}
-          alt={label}
-          width={300}
-          height={300}
-          className={`${
-            transparentBg ? 'group-hover:scale-95' : 'group-hover:scale-105'
-          } transition object-cover overflow-hidden w-full h-full`}
-        />
+    <div className='rounded-md hover:ring-2 hover:ring-zinc-200 transition hover:bg-zinc-100'>
+      <Link href={href} className='group'>
+        <div
+          className={`bg-zinc-200 group-hover:bg-zinc-300 transition rounded-t-md h-56 overflow-hidden ${
+            transparentBg && 'grid place-content-center'
+          }`}
+        >
+          <Image
+            src={src}
+            alt={label}
+            width={300}
+            height={300}
+            className={`${
+              transparentBg ? 'group-hover:scale-95' : 'group-hover:scale-105'
+            } transition object-cover overflow-hidden w-full h-full`}
+          />
+        </div>
+        <div className='p-1 group-hover:bg-zinc-100 transition rounded-md'>
+          <h3 className='font-semibold text-sm text-zinc-600'>{label}</h3>
+          <p className='font-bold text-lg'>{price} Kč</p>
+        </div>
+      </Link>
+      <div className='p-1'>
+        <AddToCartButton name={label} price={price} src={src} />
       </div>
-      <div className='p-1 group-hover:bg-zinc-100 transition rounded-md'>
-        <h3 className='font-semibold text-sm text-zinc-600'>{label}</h3>
-        <p className='font-bold text-lg'>{price} Kč</p>
-        <AddToCartButton />
-      </div>
-    </Link>
+    </div>
   )
 }
 
