eb3648a01a70d6da9cb3843d030c4342416c87f2
ton1czech <tonyasek007@gmail.com>
2023-06-25 00:08:13 +0200

    👌IMPROVE: functionality

--- components/buttons/AddToCartButton.tsx
+++ components/buttons/AddToCartButton.tsx
@@ -1,9 +1,32 @@
 import React from 'react'
 import { MdAddShoppingCart } from 'react-icons/md'
+import { useCart } from '@/hooks/useCart'
 
-const AddToCartButton = () => {
+interface AddToCartButtonProps {
+  name: string
+  price: number
+  src: string
+}
+
+const AddToCartButton: React.FC<AddToCartButtonProps> = ({
+  name,
+  price,
+  src,
+}) => {
+  const addItem = useCart(state => state.add)
+
   return (
-    <button className='flex items-center justify-center border-2 border-green text-brown bg-green hover:bg-brown hover:border-brown hover:text-zinc-100 py-1.5 px-2 rounded-md w-full gap-2 uppercase font-medium mt-5 transition max-w-[420px]'>
+    <button
+      onClick={() =>
+        addItem({
+          name,
+          price,
+          quantity: 1,
+          src,
+        })
+      }
+      className='flex items-center justify-center border-2 border-green text-brown bg-green hover:bg-brown hover:border-brown hover:text-zinc-100 py-1.5 px-2 rounded-md w-full gap-2 uppercase font-medium mt-5 transition max-w-[420px]'
+    >
       <MdAddShoppingCart size={20} /> do košíku
     </button>
   )
