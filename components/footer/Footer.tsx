5530235c9f8194897e989253ccfb1f8d36bffa24
ton1czech <tonyasek007@gmail.com>
2023-06-25 00:31:36 +0200

    👌IMPROVE: text size

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 const Footer = () => {
   return (
     <footer className='bg-green pt-20 pb-5 xl:pt-40 xl:pb-10'>
-      <p className='text-center text-light mt-12 sm:mt-4'>
+      <p className='text-center text-light text-xs md:text-base mt-12 sm:mt-4'>
         &copy;{new Date().getFullYear()}
         <Link href='https://github.com/ton1czech' className='underline'>
           {' '}
