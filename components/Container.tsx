32008e0c5c8de58a19ef1fae3e6f2987375cc464
ton1czech <tonyasek007@gmail.com>
2023-06-25 15:25:17 +0200

    👌IMPROVE: container padding

--- components/Container.tsx
+++ components/Container.tsx
@@ -6,7 +6,7 @@ interface ContainerProps {
 const Container: React.FC<ContainerProps> = ({ children, styles }) => {
   return (
     <div
-      className={`2xl:max-w-screen-2xl xl:max-w-screen-xl mx-auto px-3 xl:px-0 ${styles}`}
+      className={`2xl:max-w-screen-2xl xl:max-w-screen-xl mx-auto px-3 2xl:px-0 ${styles}`}
     >
       {children}
     </div>
