3d0810c2d509ef225e9e20553da0855a45c96983
ton1czech <tonyasek007@gmail.com>
2023-06-25 02:04:33 +0200

    👌IMPROVE: cart mobile WIP

--- components/eshop/cart/Cart.tsx
+++ components/eshop/cart/Cart.tsx
@@ -31,44 +31,83 @@ const Cart = () => {
           )}
           {cartItems.length >= 1 &&
             cartItems.map(item => (
-              <div className='flex justify-between items-center bg-zinc-300 rounded-md mb-2'>
-                <Link href={item.href}>
-                  <div className='flex justify-between items-center'>
-                    <Image
-                      src={item.src}
-                      width={150}
-                      height={150}
-                      alt={item.name}
-                    />
-                    <div>
-                      <h3>{item.name}</h3>
-                      <p>{item.price} Kč</p>
+              <>
+                <div className='hidden lg:flex justify-between items-center bg-zinc-300 rounded-md mb-2'>
+                  <Link href={item.href}>
+                    <div className='flex justify-between items-center'>
+                      <Image
+                        src={item.src}
+                        width={150}
+                        height={150}
+                        alt={item.name}
+                      />
+                      <div>
+                        <h3>{item.name}</h3>
+                        <p>{item.price} Kč</p>
+                      </div>
                     </div>
-                  </div>
-                </Link>
-                <div className='flex gap-24'>
-                  <div className='flex items-center gap-2 selection:hidden'>
-                    <AiOutlinePlus
-                      className='cursor-pointer'
-                      onClick={() => increaseQuantity(item.name)}
-                    />
+                  </Link>
+                  <div className='flex gap-24'>
+                    <div className='flex items-center gap-2 selection:hidden'>
+                      <AiOutlinePlus
+                        className='cursor-pointer'
+                        onClick={() => increaseQuantity(item.name)}
+                      />
 
-                    <p className='font-bold'>
-                      {item.quantity} {item.quantity === 1 ? 'Kus' : 'Kusů'}
-                    </p>
+                      <p className='font-bold'>
+                        {item.quantity} {item.quantity === 1 ? 'Kus' : 'Kusů'}
+                      </p>
 
-                    <AiOutlineMinus
-                      className='cursor-pointer'
-                      onClick={() => decreaseQuantity(item.name)}
+                      <AiOutlineMinus
+                        className='cursor-pointer'
+                        onClick={() => decreaseQuantity(item.name)}
+                      />
+                    </div>
+                    <IoClose
+                      size={35}
+                      className='text-red-700 mr-5 cursor-pointer'
+                      onClick={() => remove(item.name)}
                     />
                   </div>
-                  <IoClose
-                    size={35}
-                    className='text-red-700 mr-5 cursor-pointer'
-                    onClick={() => remove(item.name)}
-                  />
                 </div>
-              </div>
+
+                <div className='flex flex-col relative lg:hidden justify-between items-center bg-zinc-300 rounded-md mb-2'>
+                  <div className='flex flex-col justify-between items-center'>
+                    <Link href={item.href}>
+                      <div className='flex justify-between'>
+                        <Image
+                          src={item.src}
+                          width={150}
+                          height={150}
+                          alt={item.name}
+                        />
+                        <h3 className='font-semibold w-full'>{item.name}</h3>
+                      </div>
+                    </Link>
+                    <IoClose
+                      size={35}
+                      className='text-red-700 cursor-pointer absolute right-0 top-[50%] translate-y-[-50%]'
+                      onClick={() => remove(item.name)}
+                    />
+                    <div className='flex items-center gap-2 selection:hidden'>
+                      <AiOutlinePlus
+                        className='cursor-pointer'
+                        onClick={() => increaseQuantity(item.name)}
+                      />
+
+                      <p className='font-bold'>
+                        {item.quantity} {item.quantity === 1 ? 'Kus' : 'Kusů'}
+                      </p>
+
+                      <AiOutlineMinus
+                        className='cursor-pointer'
+                        onClick={() => decreaseQuantity(item.name)}
+                      />
+                    </div>
+                    <p className='absolute bottom-0 right-1'>{item.price} Kč</p>
+                  </div>
+                </div>
+              </>
             ))}
           {cartItems.length >= 1 && (
             <p className='text-right text-xl'>
