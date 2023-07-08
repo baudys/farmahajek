5e51032c727d019ec52773759ed47299f42fc0b3
ton1czech <tonyasek007@gmail.com>
2023-07-08 09:38:28 +0200

    🎁NEW: sluzby layout

---
+++ app/sluzby/layout.tsx
@@ -0,0 +1,9 @@
+export const metadata = {
+  title: 'Farma Hájek - Služby 🔧',
+}
+
+const SluzbyLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div>{children}</div>
+}
+
+export default SluzbyLayout
