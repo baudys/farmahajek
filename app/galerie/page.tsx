e70a7e32136920e284c065801a94fbcee93206ed
ton1czech <tonyasek007@gmail.com>
2023-07-09 20:32:19 +0200

    🎁NEW: gallery db

---
+++ app/galerie/page.tsx
@@ -0,0 +1,17 @@
+import Container from '@/components/Container'
+
+export const metadata = {
+  title: 'Farma Hájek - Galerie 🖼',
+}
+
+const GaleriePage = () => {
+  return (
+    <div className='py-28'>
+      <Container>
+        <h1>galerie</h1>
+      </Container>
+    </div>
+  )
+}
+
+export default GaleriePage
---
+++ database/gallery.ts
@@ -0,0 +1,194 @@
+export const images = [
+  {
+    src: '/gallery/1.jpg',
+  },
+  {
+    src: '/gallery/2.jpeg',
+  },
+  {
+    src: '/gallery/3.jpg',
+  },
+  {
+    src: '/gallery/4.jpeg',
+  },
+  {
+    src: '/gallery/5.jpeg',
+  },
+  {
+    src: '/gallery/6.jpeg',
+  },
+  {
+    src: '/gallery/7.jpg',
+  },
+  {
+    src: '/gallery/8.jpg',
+  },
+  {
+    src: '/gallery/9.jpg',
+  },
+  {
+    src: '/gallery/10.jpg',
+  },
+  {
+    src: '/gallery/11.jpg',
+  },
+  {
+    src: '/gallery/12.jpg',
+  },
+  {
+    src: '/gallery/13.jpg',
+  },
+  {
+    src: '/gallery/14.jpg',
+  },
+  {
+    src: '/gallery/15.jpeg',
+  },
+  {
+    src: '/gallery/16.jpeg',
+  },
+  {
+    src: '/gallery/17.jpeg',
+  },
+  {
+    src: '/gallery/18.jpeg',
+  },
+  {
+    src: '/gallery/19.jpeg',
+  },
+  {
+    src: '/gallery/20.jpeg',
+  },
+  {
+    src: '/gallery/21.jpg',
+  },
+  {
+    src: '/gallery/22.jpg',
+  },
+  {
+    src: '/gallery/23.jpg',
+  },
+  {
+    src: '/gallery/24.jpg',
+  },
+  {
+    src: '/gallery/25.jpg',
+  },
+  {
+    src: '/gallery/26.jpg',
+  },
+  {
+    src: '/gallery/27.jpg',
+  },
+  {
+    src: '/gallery/28.jpeg',
+  },
+  {
+    src: '/gallery/29.jpg',
+  },
+  {
+    src: '/gallery/30.jpg',
+  },
+  {
+    src: '/gallery/31.jpeg',
+  },
+  {
+    src: '/gallery/32.jpeg',
+  },
+  {
+    src: '/gallery/33.jpeg',
+  },
+  {
+    src: '/gallery/34.jpeg',
+  },
+  {
+    src: '/gallery/35.jpeg',
+  },
+  {
+    src: '/gallery/36.jpeg',
+  },
+  {
+    src: '/gallery/37.jpeg',
+  },
+  {
+    src: '/gallery/38.jpg',
+  },
+  {
+    src: '/gallery/39.jpeg',
+  },
+  {
+    src: '/gallery/40.jpeg',
+  },
+  {
+    src: '/gallery/41.jpeg',
+  },
+  {
+    src: '/gallery/42.jpeg',
+  },
+  {
+    src: '/gallery/43.jpeg',
+  },
+  {
+    src: '/gallery/44.jpeg',
+  },
+  {
+    src: '/gallery/45.jpeg',
+  },
+  {
+    src: '/gallery/46.jpg',
+  },
+  {
+    src: '/gallery/47.jpeg',
+  },
+  {
+    src: '/gallery/48.jpeg',
+  },
+  {
+    src: '/gallery/49.jpeg',
+  },
+  {
+    src: '/gallery/50.jpeg',
+  },
+  {
+    src: '/gallery/51.jpeg',
+  },
+  {
+    src: '/gallery/52.jpeg',
+  },
+  {
+    src: '/gallery/53.jpeg',
+  },
+  {
+    src: '/gallery/54.jpeg',
+  },
+  {
+    src: '/gallery/55.jpg',
+  },
+  {
+    src: '/gallery/56.jpg',
+  },
+  {
+    src: '/gallery/57.jpg',
+  },
+  {
+    src: '/gallery/58.jpeg',
+  },
+  {
+    src: '/gallery/59.jpeg',
+  },
+  {
+    src: '/gallery/60.jpeg',
+  },
+  {
+    src: '/gallery/61.jpg',
+  },
+  {
+    src: '/gallery/62.jpg',
+  },
+  {
+    src: '/gallery/63.jpg',
+  },
+  {
+    src: '/gallery/64.jpg',
+  },
+]
