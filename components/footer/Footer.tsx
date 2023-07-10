c456e7ec273b2c6018e94a9d251f95945e8712dc
ton1czech <tonyasek007@gmail.com>
2023-07-10 16:03:47 +0200

    🎁NEW: footer multilang

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Link from 'next/link'
 import FooterIcon from './FooterIcon'
 import { AiFillYoutube, AiOutlineInstagram, AiFillPhone } from 'react-icons/ai'
@@ -5,14 +7,25 @@ import { IoLogoTiktok } from 'react-icons/io5'
 import { FaFacebook } from 'react-icons/fa'
 import { MdEmail } from 'react-icons/md'
 import Container from '../Container'
+import { useLanguage } from '@/hooks/useLanguage'
 
 const Footer = () => {
+  const { language } = useLanguage(state => state)
+
   return (
     <footer className='bg-green pt-20 pb-5 xl:pt-32 xl:pb-10 text-brown'>
       <Container>
         <div className='flex flex-col xl:flex-row justify-between items-center gap-8 xl:gap-0'>
           <h4 className='text-2xl xl:text-4xl font-bold'>
-            Nezapomeň nás sledovat na sociálních sítích!
+            {language === 'cs' && (
+              <>Nezapomeň nás sledovat na sociálních sítích!</>
+            )}
+            {language === 'en' && (
+              <>Don't forget to follow us on social media!</>
+            )}
+            {language === 'de' && (
+              <>Vergessen Sie nicht, uns auf den sozialen Medien zu folgen!</>
+            )}
           </h4>
           <div className='flex justify-between gap-5'>
             <FooterIcon
@@ -47,43 +60,61 @@ const Footer = () => {
               <ul>
                 <li>
                   <Link href='' className='hover:underline'>
-                    Slepičky
+                    {language === 'cs' && <>Slepičky</>}
+                    {language === 'en' && <>Chickens</>}
+                    {language === 'de' && <>Hühner</>}
                   </Link>
                 </li>
                 <li>
                   <Link href='' className='hover:underline'>
-                    Kurníky
+                    {language === 'cs' && <>Kurníky</>}
+                    {language === 'en' && <>Hen houses</>}
+                    {language === 'de' && <>Hühnerställe</>}
                   </Link>
                 </li>
                 <li>
                   <Link href='' className='hover:underline'>
-                    Směsi
+                    {language === 'cs' && <>Směsi</>}
+                    {language === 'en' && <>Mixtures</>}
+                    {language === 'de' && <>Gemische</>}
                   </Link>
                 </li>
                 <li>
                   <Link href='' className='hover:underline'>
-                    Doplňky
+                    {language === 'cs' && <>Doplňky</>}
+                    {language === 'en' && <>Zubehör</>}
+                    {language === 'de' && <>Accessories</>}
                   </Link>
                 </li>
               </ul>
             </div>
 
             <div>
-              <h3 className='text-xl font-semibold pb-2'>Odkazy</h3>
+              <h3 className='text-xl font-semibold pb-2'>
+                {language === 'cs' && <>Odkazy</>}
+                {language === 'en' && <>Links</>}
+                {language === 'de' && <>Links</>}
+              </h3>
               <ul>
                 <li>
                   <Link href='kontakt' className='hover:underline'>
-                    Služby
+                    {language === 'cs' && <>Služby</>}
+                    {language === 'en' && <>Services</>}
+                    {language === 'de' && <>Dienstleistungen</>}
                   </Link>
                 </li>
                 <li>
                   <Link href='kontakt' className='hover:underline'>
-                    Kontakt
+                    {language === 'cs' && <>Kontakt</>}
+                    {language === 'en' && <>Contact</>}
+                    {language === 'de' && <>Kontakt</>}
                   </Link>
                 </li>
                 <li>
                   <Link href='Galerie' className='hover:underline'>
-                    Galerie
+                    {language === 'cs' && <>Galerie</>}
+                    {language === 'en' && <>Gallery</>}
+                    {language === 'de' && <>Gallerie</>}
                   </Link>
                 </li>
               </ul>
@@ -118,17 +149,33 @@ const Footer = () => {
           <div className='grid xl:grid-cols-2 text-center xl:text-left'>
             <div />
             <div>
-              <h3 className='text-xl font-semibold pb-2'>Otevírací doba</h3>
+              <h3 className='text-xl font-semibold pb-2'>
+                {language === 'cs' && <>Otevírací doba</>}
+                {language === 'en' && <>Opening hours</>}
+                {language === 'de' && <>Öffnungszeit</>}
+              </h3>
               <ul>
                 <li>
-                  Pátek{' '}
+                  {language === 'cs' && <>pátek </>}
+                  {language === 'en' && <>friday </>}
+                  {language === 'de' && <>freitag </>}
                   <span className='font-semibold ml-3'>16:00 - 18:00</span>
                 </li>
                 <li>
-                  Sobota{' '}
+                  {language === 'cs' && <>Sobota </>}
+                  {language === 'en' && <>Saturday </>}
+                  {language === 'de' && <>Samstag </>}
                   <span className='font-semibold ml-3'>16:00 - 18:00</span>
                 </li>
-                <li>Možnost se domluvit na jiný čas</li>
+                <li>
+                  {language === 'cs' && <>Možnost se domluvit na jiný čas</>}
+                  {language === 'en' && (
+                    <>Possibility to arrange a different time</>
+                  )}
+                  {language === 'de' && (
+                    <>Möglichkeit, einen anderen Termin zu vereinbaren</>
+                  )}
+                </li>
               </ul>
             </div>
           </div>
@@ -167,9 +214,11 @@ const Footer = () => {
           &copy;{new Date().getFullYear()}
           <Link href='<https://github.com/ton1czech>' className='underline'>
             {' '}
-            Daniel Anthony Baudyš
+            Daniel Anthony Baudyš.
           </Link>
-          . Všechna práva vyhrazena.
+          {language === 'cs' && <>Všechna práva vyhrazena.</>}
+          {language === 'en' && <>All rights reserved.</>}
+          {language === 'de' && <>Alle Rechte vorbehalten.</>}
         </p>
       </Container>
     </footer>
