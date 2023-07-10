e1eefb0c71b77f815d0fbb08cf43ff118bcbf10d
ton1czech <tonyasek007@gmail.com>
2023-07-10 15:49:51 +0200

    🎁NEW: multilangual newsletter

--- components/Newsletter.tsx
+++ components/Newsletter.tsx
@@ -3,6 +3,7 @@
 import { useState } from 'react'
 import { AiOutlineClose } from 'react-icons/ai'
 import NewsletterInput from './inputs/NewsletterInput'
+import { useLanguage } from '@/hooks/useLanguage'
 
 const Newsletter = () => {
   const [isOpen, setIsOpen] = useState(true)
@@ -10,6 +11,8 @@ const Newsletter = () => {
   const handleClick = () => {
     setIsOpen(false)
   }
+
+  const { language } = useLanguage(state => state)
 
   return (
     <div
@@ -21,11 +24,19 @@ const Newsletter = () => {
     >
       <div className='flex flex-col lg:flex-row items-center lg:justify-center lg:gap-4 h-full w-full relative'>
         <h4 className='uppercase mt-8 mb-3 lg:my-0 lg:mr-32'>
-          chcete odebírat novinky?
+          {language === 'cs' && <>chcete odebírat novinky?</>}
+          {language === 'en' && (
+            <>Would you like to subscribe to the newsletter?</>
+          )}
+          {language === 'de' && (
+            <>Möchten Sie sich für den Newsletter anmelden?</>
+          )}
         </h4>
         <NewsletterInput />
         <button className='py-1 px-1 my-4 lg:m-0 rounded-md uppercase text-lg border-2 border-brown font-semibold  hover:bg-brown text-brown hover:text-white transition'>
-          potvrdit!
+          {language === 'cs' && <>potvrdit!</>}
+          {language === 'en' && <>confirm!</>}
+          {language === 'de' && <>bestätigen!</>}
         </button>
         <AiOutlineClose
           className='absolute top-3 right-3 lg:top-[50%] lg:-translate-y-[50%] cursor-pointer'
