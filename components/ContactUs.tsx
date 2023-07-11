0e37df5a810f0b214ed732c51ecefd7aae348618
ton1czech <tonyasek007@gmail.com>
2023-07-12 00:01:38 +0200

    🎁NEW: contact button

--- components/ContactUs.tsx
+++ components/ContactUs.tsx
@@ -1,10 +1,14 @@
+import ContactSubmit from './buttons/ContactSubmit'
 import ContactInput from './inputs/ContactInput'
+import ContactTextarea from './inputs/ContactTextarea'
 
 const ContactUs = () => {
   return (
     <form className='grid content-center gap-2'>
       <ContactInput id='jmeno' label='Jméno' type='text' />
       <ContactInput id='email' label='E-mail' type='email' />
+      <ContactTextarea />
+      <ContactSubmit />
     </form>
   )
 }
---
+++ components/buttons/ContactSubmit.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ContactSubmit = () => {
+  return (
+    <button className='bg-green p-2 rounded-md placeholder:text-zinc-500 hover:scale-[99%] text-lg font-semibold transition'>
+      ODESLAT!
+    </button>
+  )
+}
+
+export default ContactSubmit
