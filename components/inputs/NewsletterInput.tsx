54c9808b15110f09db555e1e8f4a87d359ca2629
ton1czech <tonyasek007@gmail.com>
2023-07-12 00:04:21 +0200

    👌IMPROVE: newsletter input

--- components/inputs/NewsletterInput.tsx
+++ components/inputs/NewsletterInput.tsx
@@ -1,52 +1,12 @@
 'use client'
 
-import { TextField } from '@mui/material'
-import { useState } from 'react'
-
 const NewsletterInput = () => {
-  const [email, setEmail] = useState('')
-
-  const handleChange = (event: any) => {
-    setEmail(event.target.value)
-  }
-
   return (
-    <div>
-      <TextField
-        label='E-mail'
-        value={email}
-        onChange={handleChange}
-        fullWidth
-        className='rounded-md'
-        InputLabelProps={{
-          sx: {
-            color: '#3D1A04',
-            fontSize: '16px',
-            '&.Mui-focused': {
-              color: '#3D1A04',
-              fontSize: '18px',
-            },
-          },
-        }}
-        sx={{
-          '& .MuiOutlinedInput-root': {
-            '& fieldset': {
-              borderColor: '#3D1A04',
-            },
-            '&:hover fieldset': {
-              borderColor: '#3D1A04',
-            },
-            '&.Mui-focused fieldset': {
-              borderColor: '#3D1A04',
-            },
-            '& input': {
-              color: '#424242',
-              fontSize: '16px',
-            },
-          },
-        }}
-      />
-    </div>
+    <input
+      type='email'
+      placeholder='E-mail'
+      className='rounded-md py-1.5 px-1 h-10 w-64 text-zinc-900 placeholder:text-zinc-500 shadow-sm sm:text-sm sm:leading-6 focus:outline-none focus:bg-zinc-100'
+    />
   )
 }
 
