9b1acbe0bb4450aa8dd6a40095a69af3eccc507b
ton1czech <tonyasek007@gmail.com>
2023-07-12 01:05:46 +0200

    🐛BUG FIX: dynamic map import + use client on usemediaquery

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@
 
 import Companies from '@/components/Companies'
 import Hero from '@/components/Hero'
-import Map from '@/components/Map'
 import Timeline from '@/components/Timeline'
+import dynamic from 'next/dynamic'
+const Map = dynamic(() => import('@/components/Map'), { ssr: false })
 
 export default function Home() {
   return (
