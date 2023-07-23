2b64bd3cbb027f289d46df6b025398e3c5800916
ton1czech <tonyasek007@gmail.com>
2023-07-23 13:50:03 +0200

    🎁NEW: send real emails contact page

--- components/contact/ContactUs.tsx
+++ components/contact/ContactUs.tsx
@@ -1,16 +1,52 @@
 import { useLanguage } from '@/hooks/useLanguage'
-import ContactSubmit from '../buttons/ContactSubmit'
-import ContactInput from '../inputs/ContactInput'
-import ContactTextarea from '../inputs/ContactTextarea'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import emailjs from '@emailjs/browser'
+
+const schema = z.object({
+  name: z.string().min(1),
+  email: z.string().email('E-mail nesplňuje požadavky'),
+  message: z.string().min(1),
+})
 
 const ContactUs = () => {
   const { language } = useLanguage(state => state)
 
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(schema),
+  })
+
+  const onSubmit = async (data: any) => {
+    try {
+      const emailResponse = await emailjs.send(
+        process.env.NEXT_PUBLIC_SERVICE_ID!,
+        process.env.NEXT_PUBLIC_TEMPLATE_ID!,
+        data,
+        process.env.NEXT_PUBLIC_PUBLIC_API!
+      )
+
+      console.log(emailResponse)
+    } catch (error) {
+      console.log('Error sending email:', error)
+      reset()
+    }
+  }
+
   return (
-    <form className='grid content-center gap-2'>
-      <ContactInput
-        id='jmeno'
-        label={
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className='grid content-center gap-2'
+    >
+      <input
+        id='name'
+        type='text'
+        placeholder={
           language === 'cs'
             ? 'Jméno'
             : language === 'en'
@@ -19,10 +55,20 @@ const ContactUs = () => {
             ? 'Name'
             : ''
         }
-        type='text'
+        {...register('name')}
+        className='rounded-md py-1.5 px-1 text-zinc-900 placeholder:text-zinc-500 shadow-sm sm:text-sm sm:leading-6 focus:outline-none focus:bg-zinc-100'
       />
-      <ContactInput id='email' label='E-mail' type='email' />
-      <ContactTextarea
+      {errors.name && <div className='text-red-500'>Zadejte vaše jméno</div>}
+      <input
+        id='email'
+        placeholder='E-mail'
+        type='email'
+        {...register('email')}
+        className='rounded-md py-1.5 px-1 text-zinc-900 placeholder:text-zinc-500 shadow-sm sm:text-sm sm:leading-6 focus:outline-none focus:bg-zinc-100'
+      />
+      {errors.email && <div className='text-red-500'>Neplatný E-mail</div>}
+      <textarea
+        id='message'
         placeholder={
           language === 'cs'
             ? 'Zadejte dotaz'
@@ -32,18 +78,17 @@ const ContactUs = () => {
             ? 'Eine Frage stellen'
             : ''
         }
+        cols={3}
+        rows={8}
+        {...register('message')}
+        className='rounded-md py-1.5 px-1 text-zinc-900 resize-none focus:outline-none focus:bg-zinc-100 transition placeholder:text-zinc-500'
       />
-      <ContactSubmit
-        label={
-          language === 'cs'
-            ? 'ODESLAT!'
-            : language === 'en'
-            ? 'SEND!'
-            : language === 'de'
-            ? 'SENDEN SIE ES!'
-            : ''
-        }
-      />
+      {errors.message && <div className='text-red-500'>Zadejte zprávu</div>}
+      <button className='bg-green p-2 rounded-md placeholder:text-zinc-500 hover:scale-[99%] text-lg font-semibold transition'>
+        {language === 'cs' && <> ODESLAT!</>}
+        {language === 'en' && <> SEND!</>}
+        {language === 'de' && <> SENDEN SIE ES!</>}
+      </button>
     </form>
   )
 }
