bb1971c1a8a43c51c5a3feb80e139e53ccfc9dc3
ton1czech <tonyasek007@gmail.com>
2023-07-25 10:16:28 +0200

    👌IMPROVE: real footer links

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -14,11 +14,11 @@ const Footer = () => {
   const { language } = useLanguage(state => state)
 
   return (
-    <footer className='bg-green pt-20 pb-5 xl:pt-32 xl:pb-10 text-brown'>
+    <footer className='pt-20 pb-5 bg-green xl:pt-32 xl:pb-10 text-brown'>
       <Container>
-        <div className='flex flex-col xl:flex-row justify-between items-center gap-8 xl:gap-0'>
+        <div className='flex flex-col items-center justify-between gap-8 xl:flex-row xl:gap-0'>
           <Fade left>
-            <h4 className='text-2xl xl:text-4xl font-bold'>
+            <h4 className='text-2xl font-bold xl:text-4xl'>
               {language === 'cs' && (
                 <>Nezapomeň nás sledovat na sociálních sítích!</>
               )}
@@ -41,7 +41,7 @@ const Footer = () => {
                 icon={IoLogoTiktok}
               />
               <FooterIcon
-                href='[https://www.facebook.com/people/Farma-Hájek/100092479713291/](https://www.facebook.com/people/Farma-H%C3%A1jek/100092479713291/)'
+                href='https://www.facebook.com/people/Farma-Hájek/100092479713291/'
                 icon={FaFacebook}
               />
               <FooterIcon
@@ -53,42 +53,54 @@ const Footer = () => {
         </div>
 
         <Fade right>
-          <div className='h-px w-full bg-brown/60 my-10' />
+          <div className='w-full h-px my-10 bg-brown/60' />
         </Fade>
 
         <div className='grid xl:grid-cols-3'>
-          <div className='flex items-start justify-center xl:justify-start gap-16'>
+          <div className='flex items-start justify-center gap-16 xl:justify-start'>
             <Fade top>
               <div>
-                <Link href=''>
-                  <h3 className='text-xl font-semibold pb-2 hover:underline'>
+                <Link href='https://www.eshop-farmahajek.cz/'>
+                  <h3 className='pb-2 text-xl font-semibold hover:underline'>
                     E-shop
                   </h3>
                 </Link>
                 <ul>
                   <li>
-                    <Link href='' className='hover:underline'>
+                    <Link
+                      href='https://www.eshop-farmahajek.cz/slepicky/'
+                      className='hover:underline'
+                    >
                       {language === 'cs' && <>Slepičky</>}
                       {language === 'en' && <>Chickens</>}
                       {language === 'de' && <>Hühner</>}
                     </Link>
                   </li>
                   <li>
-                    <Link href='' className='hover:underline'>
+                    <Link
+                      href='https://www.eshop-farmahajek.cz/kurniky/'
+                      className='hover:underline'
+                    >
                       {language === 'cs' && <>Kurníky</>}
                       {language === 'en' && <>Hen houses</>}
                       {language === 'de' && <>Hühnerställe</>}
                     </Link>
                   </li>
                   <li>
-                    <Link href='' className='hover:underline'>
+                    <Link
+                      href='https://www.eshop-farmahajek.cz/smesi-a-doplnky/'
+                      className='hover:underline'
+                    >
                       {language === 'cs' && <>Směsi</>}
                       {language === 'en' && <>Mixtures</>}
                       {language === 'de' && <>Gemische</>}
                     </Link>
                   </li>
                   <li>
-                    <Link href='' className='hover:underline'>
+                    <Link
+                      href='https://www.eshop-farmahajek.cz/doplnky/'
+                      className='hover:underline'
+                    >
                       {language === 'cs' && <>Doplňky</>}
                       {language === 'en' && <>Zubehör</>}
                       {language === 'de' && <>Accessories</>}
@@ -100,28 +112,37 @@ const Footer = () => {
 
             <Fade top>
               <div>
-                <h3 className='text-xl font-semibold pb-2'>
+                <h3 className='pb-2 text-xl font-semibold'>
                   {language === 'cs' && <>Odkazy</>}
                   {language === 'en' && <>Links</>}
                   {language === 'de' && <>Links</>}
                 </h3>
                 <ul>
                   <li>
-                    <Link href='kontakt' className='hover:underline'>
+                    <Link
+                      href='https://farmahajek.cz/sluzby'
+                      className='hover:underline'
+                    >
                       {language === 'cs' && <>Služby</>}
                       {language === 'en' && <>Services</>}
                       {language === 'de' && <>Dienstleistungen</>}
                     </Link>
                   </li>
                   <li>
-                    <Link href='kontakt' className='hover:underline'>
+                    <Link
+                      href='https://farmahajek.cz/kontakt'
+                      className='hover:underline'
+                    >
                       {language === 'cs' && <>Kontakt</>}
                       {language === 'en' && <>Contact</>}
                       {language === 'de' && <>Kontakt</>}
                     </Link>
                   </li>
                   <li>
-                    <Link href='Galerie' className='hover:underline'>
+                    <Link
+                      href='https://farmahajek.cz/galerie'
+                      className='hover:underline'
+                    >
                       {language === 'cs' && <>Galerie</>}
                       {language === 'en' && <>Gallery</>}
                       {language === 'de' && <>Gallerie</>}
@@ -133,16 +154,16 @@ const Footer = () => {
           </div>
 
           <Fade left>
-            <div className='xl:hidden h-px w-full bg-brown/60 my-10' />
+            <div className='w-full h-px my-10 xl:hidden bg-brown/60' />
           </Fade>
 
           <Fade top>
-            <div className='bg-brown p-4 text-zinc-400 rounded-md shadow hidden xl:block'>
-              <h2 className='text-white font-semibold mb-5 text-lg'>
+            <div className='hidden p-4 rounded-md shadow bg-brown text-zinc-400 xl:block'>
+              <h2 className='mb-5 text-lg font-semibold text-white'>
                 Miloslav Polák Ml.
               </h2>
 
-              <div className='mb-7 text-zinc-200 ml-6'>
+              <div className='ml-6 mb-7 text-zinc-200'>
                 <p className='flex gap-2'>
                   <AiFillPhone />
                   +420 720 566 212
@@ -153,7 +174,7 @@ const Footer = () => {
                 </p>
               </div>
 
-              <div className='text-center text-sm'>
+              <div className='text-sm text-center'>
                 <p>Hájek 36</p>
                 <p>34506 Všeruby - Hájek</p>
                 <p>okres Domažlice, Plzeňský kraj, Česko</p>
@@ -162,10 +183,10 @@ const Footer = () => {
           </Fade>
 
           <Fade top>
-            <div className='grid xl:grid-cols-2 text-center xl:text-left'>
+            <div className='grid text-center xl:grid-cols-2 xl:text-left'>
               <div />
               <div>
-                <h3 className='text-xl font-semibold pb-2'>
+                <h3 className='pb-2 text-xl font-semibold'>
                   {language === 'cs' && <>Otevírací doba</>}
                   {language === 'en' && <>Opening hours</>}
                   {language === 'de' && <>Öffnungszeit</>}
@@ -175,13 +196,13 @@ const Footer = () => {
                     {language === 'cs' && <>pátek </>}
                     {language === 'en' && <>friday </>}
                     {language === 'de' && <>freitag </>}
-                    <span className='font-semibold ml-3'>16:00 - 18:00</span>
+                    <span className='ml-3 font-semibold'>16:00 - 18:00</span>
                   </li>
                   <li>
                     {language === 'cs' && <>Sobota </>}
                     {language === 'en' && <>Saturday </>}
                     {language === 'de' && <>Samstag </>}
-                    <span className='font-semibold ml-3'>16:00 - 18:00</span>
+                    <span className='ml-3 font-semibold'>16:00 - 18:00</span>
                   </li>
                   <li>
                     {language === 'cs' && <>Možnost se domluvit na jiný čas</>}
@@ -198,17 +219,17 @@ const Footer = () => {
           </Fade>
         </div>
         <Fade right>
-          <div className='h-px w-full bg-brown/60 my-10 xl:hidden' />
+          <div className='w-full h-px my-10 bg-brown/60 xl:hidden' />
         </Fade>
 
         <Fade top>
           <div className='grid place-items-center xl:hidden'>
-            <div className='bg-brown p-4 text-zinc-400 rounded-md shadow'>
-              <h2 className='text-white font-semibold mb-5 text-lg'>
+            <div className='p-4 rounded-md shadow bg-brown text-zinc-400'>
+              <h2 className='mb-5 text-lg font-semibold text-white'>
                 Miloslav Polák Ml.
               </h2>
 
-              <div className='mb-7 text-zinc-200 ml-6'>
+              <div className='ml-6 mb-7 text-zinc-200'>
                 <p className='flex gap-2'>
                   <AiFillPhone />
                   +420 720 566 212
@@ -219,7 +240,7 @@ const Footer = () => {
                 </p>
               </div>
 
-              <div className='text-center text-sm'>
+              <div className='text-sm text-center'>
                 <p>Hájek 36</p>
                 <p>34506 Všeruby - Hájek</p>
                 <p>okres Domažlice, Plzeňský kraj, Česko</p>
@@ -229,18 +250,18 @@ const Footer = () => {
         </Fade>
 
         <Fade left>
-          <div className='h-px w-full bg-brown/60 my-10' />
+          <div className='w-full h-px my-10 bg-brown/60' />
         </Fade>
 
         <Fade bottom>
-          <div className='flex flex-col lg:flex-row justify-center gap-20'>
-            <p className='text-center text-light text-xs md:text-base mt-12 sm:mt-4'>
+          <div className='flex flex-col justify-center gap-20 lg:flex-row'>
+            <p className='mt-12 text-xs text-center text-light md:text-base sm:mt-4'>
               &copy;{new Date().getFullYear()} Farmahájek.
               {language === 'cs' && <> Všechna práva vyhrazena.</>}
               {language === 'en' && <> All rights reserved.</>}
               {language === 'de' && <> Alle Rechte vorbehalten.</>}
             </p>
-            <p className='text-center text-light text-xs md:text-base mt-12 sm:mt-4 bg-brown text-green px-2 py-1 rounded-md'>
+            <p className='px-2 py-1 mt-12 text-xs text-center rounded-md text-light md:text-base sm:mt-4 bg-brown text-green'>
               {language === 'cs' && <>Vytvořil </>}
               {language === 'en' && <>Created by </>}
               {language === 'de' && <>Erstellt von </>}
