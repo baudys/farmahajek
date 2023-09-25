75a1c4397341ed1212ae48575c72b96a50cd9574
ton1czech <tonyasek007@gmail.com>
2023-09-25 11:06:35 +0200

    🎁NEW: upload hero video to imgur to reduce vercel bandwith usage

--- components/Video.tsx
+++ components/Video.tsx
@@ -18,10 +18,11 @@ const Video = () => {
       loop
       muted
       playsInline
-      className='absolute h-full w-full object-cover'
+      className='absolute object-cover w-full h-full'
     >
-      <source src='/hero.mp4' type='video/mp4; codecs=hvc1' />
-      <source src='/hero.mp4' type='video/mp4; codecs=avc1.42E01E' />
+      {/* <source src='/hero.mp4' type='video/mp4; codecs=hvc1' />
+      <source src='/hero.mp4' type='video/mp4; codecs=avc1.42E01E' /> */}
+      <source src='https://i.imgur.com/1Ml98Cw.mp4' />
     </video>
   )
 }
