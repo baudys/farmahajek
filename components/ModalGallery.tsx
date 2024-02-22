227ad49ed69707905e1658ea0b3bfc53c58ef838
ton1czech <tonyasek007@gmail.com>
2024-02-22 12:34:51 +0100

    🐛BUG FIX: gallery layout

--- components/ModalGallery.tsx
+++ components/ModalGallery.tsx
@@ -5,18 +5,15 @@ import ModalImage from 'react-modal-image'
 interface ModalGalleryProps {
   images: any[]
   tall?: boolean
-  square?: boolean
 }
 
-const ModalGallery = ({ images, tall, square }: ModalGalleryProps) => {
+const ModalGallery = ({ images, tall }: ModalGalleryProps) => {
   return (
     <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4'>
       {images.map(image => (
         <div
-          className={`h-56 overflow-hidden md:h-52 lg:h-44 xl:h-52 ${
+          className={`relative h-56 overflow-hidden md:h-52 lg:h-44 xl:h-52 ${
             tall && '!h-full !w-full overflow-hidden aspect-[3/4]'
-          } ${
-            square && '!h-full !w-full overflow-hidden aspect-square relative'
           }`}
         >
           <ModalImage
