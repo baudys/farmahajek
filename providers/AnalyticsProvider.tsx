246005eec5273214cd6d445aec0af9add78856f6
ton1czech <tonyasek007@gmail.com>
2024-04-22 08:42:12 +0200

    🐛BUG FIX: using localstorage to check if cookies enabled

--- providers/AnalyticsProvider.tsx
+++ providers/AnalyticsProvider.tsx
@@ -3,31 +3,43 @@
 import { useCookies } from '@/hooks/useCookies'
 import { GoogleAnalytics } from '@next/third-parties/google'
 import Script from 'next/script'
+import { useEffect, useState } from 'react'
 
 export const AnalyticsProvider = () => {
   const { cookiesEnabled } = useCookies()
 
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  })
+
   return (
     <>
-      {cookiesEnabled ||
-        (window.localStorage
-          .getItem('cookies-storage')
-          ?.includes('"cookiesEnabled":true') && (
-          <>
-            <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GTAG!} />
-            <Script
-              async
-              src='https://www.googletagmanager.com/gtag/js?id=G-XZY4E8TSVH'
-            />
-            <Script>
-              {`
-        window.dataLayer = window.dataLayer || []; 
-        function gtag(){dataLayer.push(arguments);} 
-        gtag('js', new Date()); 
-        gtag('config', 'G-XZY4E8TSVH');`}
-            </Script>
-          </>
-        ))}
+      {mounted && (
+        <>
+          {cookiesEnabled ||
+            (window &&
+              window.localStorage
+                .getItem('cookies-storage')
+                ?.includes('"cookiesEnabled":true') && (
+                <>
+                  <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GTAG!} />
+                  <Script
+                    async
+                    src='https://www.googletagmanager.com/gtag/js?id=G-XZY4E8TSVH'
+                  />
+                  <Script>
+                    {`
+                        window.dataLayer = window.dataLayer || []; 
+                        function gtag(){dataLayer.push(arguments);} 
+                        gtag('js', new Date()); 
+                        gtag('config', 'G-XZY4E8TSVH');`}
+                  </Script>
+                </>
+              ))}
+        </>
+      )}
     </>
   )
 }
