98d04109603eacc8c424a1de7417ea3f18da09e2
ton1czech <tonyasek007@gmail.com>
2024-09-20 13:01:41 +0200

    👌IMPROVE: sklik

--- providers/SklikProvider.tsx
+++ providers/SklikProvider.tsx
@@ -1,8 +1,6 @@
 'use client'
 
 import { useCookies } from '@/hooks/useCookies'
-import Head from 'next/head'
-import Script from 'next/script'
 import { useEffect, useState } from 'react'
 
 export const SklikProvider = () => {
@@ -22,25 +20,25 @@ export const SklikProvider = () => {
             window?.localStorage
               .getItem('cookies-storage')
               ?.includes('"cookiesEnabled":true')) && (
-            <Head>
+            <head>
               <script
                 type='text/javascript'
                 src='https://c.seznam.cz/js/rc.js'
               />
-              <script id='retargeting-script'>
+              <script>
                 {`
                     window.sznIVA.IS.updateIdentities({
                         eid: null
                     });
 
                     var retargetingConf = {
-                        rtgId: ${process.env.NEXT_PUBLIC_SEZNAM!},
+                        rtgId: 1355943,
                         consent: null
                     };
                     window.rc.retargetingHit(retargetingConf);
                 `}
               </script>
-            </Head>
+            </head>
           )}
         </>
       )}
