1d86c911f834a322efc8c8c86bcfe71ed1531a33
ton1czech <tonyasek007@gmail.com>
2025-03-15 11:17:26 +0100

    🎁NEW: konfigurator wip

---
+++ app/konfigurator/page.tsx
@@ -0,0 +1,98 @@
+'use client'
+
+import Container from '@/components/container'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+import { slepicky } from '@/database/slepicky'
+import { useState } from 'react'
+
+export default function KonfiguratorPage() {
+  const [barva, setBarva] = useState<string>()
+
+  const filteredSlepicky = barva
+    ? slepicky.filter((slepicka) => slepicka.color === barva)
+    : []
+
+  const selectedSlepicka =
+    filteredSlepicky.length > 0
+      ? filteredSlepicky[Math.floor(Math.random() * filteredSlepicky.length)]
+      : null
+
+  return (
+    <main className='pb-28'>
+      <div className='h-[70.72px] w-full bg-black xl:h-[84.81px]' />
+      <Container className='mt-10'>
+        <h1 className='text-3xl font-bold md:text-4xl lg:text-5xl'>
+          Najděte si perfektní slepičku
+        </h1>
+        <p className='mt-1 max-w-prose'>
+          Využijte náš nový konfigurátor pro nalezení perfektní slepičky přímo
+          pro Vás. Navolte si požadavky a Vaše vyvolená slepička se zobrazí na
+          obrazovce.
+        </p>
+
+        <section className='mt-10'>
+          <div>
+            <Select onValueChange={(value) => setBarva(value)}>
+              <SelectTrigger className='w-[180px]'>
+                <SelectValue placeholder='Barva Skořápky' />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value='bila'>Bílá</SelectItem>
+                <SelectItem value='hneda'>Hnědá</SelectItem>
+                <SelectItem value='kremova'>Krémová</SelectItem>
+                <SelectItem value='modra'>Modrá</SelectItem>
+                <SelectItem value='zelena'>Zelená</SelectItem>
+                <SelectItem value='tmave-hneda'>Tmavě Hnědá</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+
+          <div className='mt-6 text-center'>
+            {barva ? (
+              selectedSlepicka ? (
+                <>
+                  <h2 className='text-xl font-semibold'>
+                    {selectedSlepicka.name}
+                  </h2>
+                  <img
+                    src={selectedSlepicka.img}
+                    alt={selectedSlepicka.name}
+                    className='mx-auto max-w-xs'
+                  />
+                </>
+              ) : (
+                <>
+                  <h2 className='text-lg text-gray-500'>
+                    Nevyhovuje žádná slepička
+                  </h2>
+                  <img
+                    src='/konfigurator/random.webp'
+                    alt='Obrys slepičky'
+                    className='mx-auto max-w-xs opacity-50'
+                  />
+                </>
+              )
+            ) : (
+              <>
+                <h2 className='text-lg text-gray-500'>
+                  Vyplňte alespoň jeden parametr
+                </h2>
+                <img
+                  src='/konfigurator/random.webp'
+                  alt='Obrys slepičky'
+                  className='mx-auto max-w-xs opacity-50'
+                />
+              </>
+            )}
+          </div>
+        </section>
+      </Container>
+    </main>
+  )
+}
---
+++ database/slepicky.ts
@@ -0,0 +1,26 @@
+export const slepicky = [
+  {
+    name: 'D102',
+    color: 'hneda',
+    img: '/konfigurator/d102.webp',
+    url: 'https://www.eshop-farmahajek.cz/dominant-hnedy-d102/',
+  },
+  {
+    name: 'D301',
+    color: 'kremova',
+    img: '/konfigurator/d301.webp',
+    url: 'https://www.eshop-farmahajek.cz/dominant-tricolor-d301/',
+  },
+  {
+    name: 'D601',
+    color: 'bila',
+    img: '/konfigurator/d601.webp',
+    url: 'https://www.eshop-farmahajek.cz/dominant-leghorn-cernobily-d601/',
+  },
+  {
+    name: 'D843',
+    color: 'hneda',
+    img: '/konfigurator/d843.webp',
+    url: 'https://www.eshop-farmahajek.cz/d843/',
+  },
+]
